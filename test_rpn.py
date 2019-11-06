4107ee4aeb025818b1bf71e746231571aa4f3f27
Julian Jacobs <juja@umich.edu>
2019-11-06 18:26:52 -0500

    new repository

---
+++ test_rpn.py
@@ -0,0 +1,8 @@
+rpn.py + test_rpn.py
+import uniteset
+import rpn
+
+class TestBasics(unittest.TestCase):
+    def test_add(self):
+        result = rpn.calculate('1 1 +')
+        self.assertEqual(2, result)
